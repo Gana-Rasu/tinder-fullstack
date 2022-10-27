287353e0ee1dc4e2f87418e8c62900e9771ecef5
Gana <gananathanthirunavukkarasu@gmail.com>
2022-10-27 11:20:13 +0530

    dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,93 @@
-import React from "react";
+import React,{useState} from "react";
+// import TinderCard from "react-tinder-card";
+import { CardSwiper } from "react-card-rotate-swiper";
+import ChatContainer from "../components/ChatContainer";
 
 function Dashboard() {
-    return (
-      <div className="Dashboard">
-        Dashboard
+  const characters = [
+    {
+      name: "Richard Hendricks",
+      url: "https://i.imgur.com/oPj4A8u.jpeg",
+    },
+    {
+      name: "Erlich Bachman",
+      url: "https://i.imgur.com/Q9WPlWA.jpeg",
+    },
+    {
+      name: "Monica Hall",
+      url: "https://i.imgur.com/oPj4A8u.jpeg",
+    },
+    {
+      name: "Jared Dunn",
+      url: "https://i.imgur.com/oPj4A8u.jpeg",
+    },
+    {
+      name: "Dinesh Chugtai",
+      url: "https://i.imgur.com/oPj4A8u.jpeg",
+    },
+  ];
+  const [lastDirection, setLastDirection] = useState();
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete,direction);
+    setLastDirection(direction);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!");
+  };
+
+  // const handleSwipe = (d) => {
+  //   console.log("You swiped: " + d);
+  // };
+
+  return (
+    <div className="dashboard">
+      <ChatContainer/>
+      <div className="swipe-container">
+        <div className="card-container">
+          {/* {characters.map((character) => (
+          <TinderCard
+            className="swipe"
+            key={character.name}
+            onSwipe={(dir) => swiped(dir, character.name)}
+            onCardLeftScreen={() => outOfFrame(character.name)}
+          >
+            <div
+              style={{ backgroundImage: "url(" + character.url + ")" }}
+              className="card"
+            >
+              <h3>{character.name}</h3>
+            </div>
+          </TinderCard>
+        ))} */}
+
+          {characters.map((character) => (
+            <CardSwiper
+            className="swipe"
+              key={character.name}
+              onSwipe={(dir) => swiped(dir, character.name)}
+              onCardLeftScreen={() => outOfFrame(character.name)}
+              // onSwipe={handleSwipe}
+              preventSwipe={["up", "down"]}
+              contents={
+                <div
+                style={{ backgroundImage: "url(" + character.url + ")" }}
+                  className="card"
+                >
+                  <h3>{character.name}</h3>
+                </div>
+              }
+            />
+          ))}
+            <div className="swipe-info" >
+              {lastDirection ? <p>you swiped {lastDirection}</p> : <p/> }
+            </div>
+
+        </div>
       </div>
-    );
-  }
-  
-  export default Dashboard;
-  
+    </div>
+  );
+}
+
+export default Dashboard;
